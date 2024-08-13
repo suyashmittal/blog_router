0feea086cc81b551930a7e2762a93d7128e7f472
Suyash Mittal <suyashmittal1@gmail.com>
2024-08-14 00:04:15 +0530

    used contextAPI for state management

--- src/NewPost.js
+++ src/NewPost.js
@@ -1,4 +1,30 @@
-const NewPost = ({ handleSubmit, postTitle, setPostTitle, postBody, setPostBody }) => {
+import { useState, useContext } from 'react';
+import api from './api/posts';
+import { format } from 'date-fns';
+import DataContext from './context/DataContext';
+
+const NewPost = () => {
+    const [postTitle, setPostTitle] = useState('');
+    const [postBody, setPostBody] = useState('');
+    const { posts, setPosts, history } = useContext(DataContext);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+        const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+        const newPost = { id, title: postTitle, datetime, body: postBody };
+        try {
+            const response = await api.post('/posts', newPost);
+            const allPosts = [...posts, response.data];
+            setPosts(allPosts);
+            setPostTitle('');
+            setPostBody('');
+            history.push('/');
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+        }
+    }
+
     return (
         <main className='NewPost'>
             <h2>New Post</h2>
--- src/PostPage.js
+++ src/PostPage.js
@@ -1,8 +1,24 @@
+import { useContext } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import api from './api/posts';
+import DataContext from './context/DataContext';
 
-const PostPage = ({ posts, handleDelete }) => {
+const PostPage = () => {
+    const { posts, setPosts, history } = useContext(DataContext);
     const { id } = useParams();
     const post = posts.find(post => (post.id).toString() === id);
+
+    const handleDelete = async (id) => {
+        try {
+            await api.delete(`/posts/${id}`);
+            const filteredPosts = posts.filter(post => (post.id !== id));
+            setPosts(filteredPosts);
+            history.push('/');
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+        }
+    }
+
     return (
         <main className="PostPage">
             <article className="post">
